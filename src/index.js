e87394042600bb90e2f51d92f052f76ff18d99b5
spice2445 <79026145+spice2445@users.noreply.github.com>
2021-03-08 01:03:06 +0200

    init: start reverse int

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 module.exports = function reverse (n) {
-  return n.split('').reverse().join(''); 
-    if(n<=0){
-        return 0 
-    }
+  let reverseNum = n.toString().split('').reverse().join('');
+  let num = Math.sign(n) * parseInt(reverseNum);
+
+  if ( n < 0 ) {
+      return num * -1;
+  }
+
+  return num;
 }
